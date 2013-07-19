aa5a0d3c245920bbc012d059e0d669f0d94a298d
Felipe cruz <felipecruz@loogica.net>
2013-07-19 17:31:56 -0300

    Remove UI

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,20 @@
 function NewsController($scope, $http) {
     $scope.news = [];
+
+    // this should be done dy DI
+    $scope.moment = moment;
+
     $scope.update = function() {
         $http({
             url: '/api/news',
             method: 'GET'
         }).success(function(data, status, header, config) {
             $scope.news = data.items;
+            $scope.show_items = true;
         }).error(function(data, status, header, config) {
             alert('API ERROR');
         });
-    }
-    $scope.update();
+    };
 
     $scope.vote = function(item_id) {
         $http({
@@ -20,8 +24,19 @@ function NewsController($scope, $http) {
             $scope.news = data.items;
         }).error(function(data, status, header, config) {
             alert('API ERROR');
-        })
-    }
+        });
+    };
 
-    $scope.moment = moment;
+    $scope.remove = function(item_id) {
+        $http({
+            url: '/api/remove/' + item_id,
+            method: 'GET'
+        }).success(function(data, status, header, config) {
+            $scope.news = data.items;
+        }).error(function(data, status, header, config) {
+            alert('API ERROR');
+        });
+    };
+
+    $scope.update();
 }
