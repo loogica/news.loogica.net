0d3294db8126a35e4eea35244dfae2152eabe208
Felipe cruz <felipecruz@loogica.net>
2013-08-02 19:29:46 -0300

    Add angular "looginews" module

--- static/js/main.js
+++ static/js/main.js
@@ -1,69 +1,73 @@
-function NewsController($scope, $http) {
-    // this should be done dy DI
-    moment.lang('pt-br');
-    $scope.moment = moment;
+var news = angular.module('looginews', []);
 
-    $scope.news = [];
-    $scope.channel = channel;
-    $scope._date_sort_flag = true;
+news.controller('NewsController',
+    function($scope, $http) {
+        // this should be done dy DI
+        moment.lang('pt-br');
+        $scope.moment = moment;
 
-    var _process_items = function(items) {
-        _.each(items, function(item) {
-            item.date_posted = moment(item.posted);
-        });
-        return items;
-    };
-
-    $scope.update = function() {
-        $http({
-            url: '/api/news/' + $scope.channel,
-            method: 'GET'
-        }).success(function(data, status, header, config) {
-            $scope.news = _process_items(data.items);
-            $scope.show_items = true;
-        }).error(function(data, status, header, config) {
-            alert('API ERROR');
-        });
-    };
-
-    $scope.vote = function(item_id) {
-        $http({
-            url: '/api/vote/' + $scope.channel + '/' + item_id,
-            method: 'GET'
-        }).success(function(data, status, header, config) {
-            $scope.news = _process_items(data.items);
-        }).error(function(data, status, header, config) {
-            alert('API ERROR');
-        });
-    };
-
-    $scope.remove = function(item_id) {
-        $http({
-            url: '/api/remove/' + $scope.channel + '/' + item_id,
-            method: 'GET'
-        }).success(function(data, status, header, config) {
-            $scope.news = _process_items(data.items);
-        }).error(function(data, status, header, config) {
-            alert('API ERROR');
-        });
-    };
-
-    $scope.sort_date = function() {
-         var items = _.sortBy($scope.news, function(element) {
-            return element.date_posted;
-        });
-        if ($scope._date_sort_flag) {
-            $scope.news = items.reverse();
-        } else {
-            $scope.news = items;
-        }
-        $scope._date_sort_flag = !$scope._date_sort_flag;
-    };
-
-    $scope.open = function(channel) {
+        $scope.news = [];
         $scope.channel = channel;
-        $scope.update();
-    };
+        $scope._date_sort_flag = true;
 
-    $scope.update();
-}
+        var _process_items = function(items) {
+            _.each(items, function(item) {
+                item.date_posted = moment(item.posted);
+            });
+            return items;
+        };
+
+        $scope.update = function() {
+            $http({
+                url: '/api/news/' + $scope.channel,
+                method: 'GET'
+            }).success(function(data, status, header, config) {
+                $scope.news = _process_items(data.items);
+                $scope.show_items = true;
+            }).error(function(data, status, header, config) {
+                alert('API ERROR');
+            });
+        };
+
+        $scope.vote = function(item_id) {
+            $http({
+                url: '/api/vote/' + $scope.channel + '/' + item_id,
+                method: 'GET'
+            }).success(function(data, status, header, config) {
+                $scope.news = _process_items(data.items);
+            }).error(function(data, status, header, config) {
+                alert('API ERROR');
+            });
+        };
+
+        $scope.remove = function(item_id) {
+            $http({
+                url: '/api/remove/' + $scope.channel + '/' + item_id,
+                method: 'GET'
+            }).success(function(data, status, header, config) {
+                $scope.news = _process_items(data.items);
+            }).error(function(data, status, header, config) {
+                alert('API ERROR');
+            });
+        };
+
+        $scope.sort_date = function() {
+             var items = _.sortBy($scope.news, function(element) {
+                return element.date_posted;
+            });
+            if ($scope._date_sort_flag) {
+                $scope.news = items.reverse();
+            } else {
+                $scope.news = items;
+            }
+            $scope._date_sort_flag = !$scope._date_sort_flag;
+        };
+
+        $scope.open = function(channel) {
+            $scope.channel = channel;
+            $scope.update();
+        };
+
+        $scope.update();
+    }
+);
